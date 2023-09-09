a26fb9aeb49cdc0bbb7796919fbf08a4d6da9b0a
NorahSaad <143969550+NorahSaad@users.noreply.github.com>
2023-09-10 00:13:10 +0300

    Add files via upload

---
+++ Calculate remining age  (1).py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #The maximum is 90
+
+# In[4]:
+
+
+
+x=int(input(" Enter your age:\n"))
+
+age=90-x
+month=age*12
+week=age*52
+days=age*365
+print(f"you will life about : {age} years {month} months {week} weeks and {days}days")
+
+
+# In[ ]:
+
+
+
+
